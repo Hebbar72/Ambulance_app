85757f9b63864e9d5d1b5681e6f99f8e07f679e0
Hebbar72 <103434388+Hebbar72@users.noreply.github.com>
2022-06-29 18:40:17 +0530

    Add files via upload

---
+++ mapmyindia_gl/generated/source/buildConfig/debug/com/mapmyindia/mapmyindiagl/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.mapmyindia.mapmyindiagl;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.mapmyindia.mapmyindiagl";
+  public static final String BUILD_TYPE = "debug";
+}
